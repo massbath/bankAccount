d110058702f48c76fbc6cf89677265920b6ef929
massbath <clement.vassant@gmail.com>
2020-03-09 11:16:54 +0100

    Rearrange test to remove initial balance from given parameters

--- src/test/java/com/vassant/kata/domain/BankAccountAcceptanceTest.java
+++ src/test/java/com/vassant/kata/domain/BankAccountAcceptanceTest.java
@@ -23,44 +23,22 @@ public class BankAccountAcceptanceTest {
     private Operations operations = new OperationsInMemory();
     private BankAccountOperations bankAccount;
 
-    @Test
-    @Parameters({"0,100,100",
-            "-100,100,0"
-    })
-    public void a_deposit_in_a_account_should_be_add_in_balance(int initialBalance, int amountOfDeposit, int finaleBalance) {
-        bankAccount = new BankAccount(Balance.of(initialBalance), operations, fakeClock);
 
-        bankAccount.deposit(Amount.of(amountOfDeposit));
-
-        assertThat(bankAccount.balance()).isEqualTo(Balance.of(finaleBalance));
-    }
-
-    @Test
-    @Parameters({"100,100,0",
-            "100,100,0"
-    })
-    public void withdraw_some_amount_from_account_should_be_remove_from_balance(int initialBalance, int amountOfWithDraw, int expectedBalance) {
-        bankAccount = new BankAccount(Balance.of(initialBalance), operations, fakeClock);
-
-        bankAccount.withdraw(Amount.of(amountOfWithDraw));
-
-        assertThat(bankAccount.balance()).isEqualTo(Balance.of(expectedBalance));
-    }
 
 
     @Test
     public void all_operations_on_account_should_appear_in_the_history() {
-        int initialBalance = 500;
+        int initialBalance = 0;
         bankAccount = new BankAccount(Balance.of(initialBalance), operations, fakeClock);
         final Amount aAmountOf100 = Amount.of(100);
 
         History historyExpected = History.from(asList(
-                Operation.builder().operationType(OperationType.WITHDRAW).date(TODAY).amount(aAmountOf100).balance(Balance.of(400)).build(),
-                Operation.builder().operationType(OperationType.DEPOSIT).date(TODAY).amount(aAmountOf100).balance(Balance.of(500)).build(),
-                Operation.builder().operationType(OperationType.WITHDRAW).date(TODAY).amount(aAmountOf100).balance(Balance.of(400)).build(),
-                Operation.builder().operationType(OperationType.DEPOSIT).date(TODAY).amount(aAmountOf100).balance(Balance.of(500)).build()));
+                Operation.builder().operationType(OperationType.DEPOSIT).date(TODAY).amount(aAmountOf100).balance(Balance.of(100)).build(),
+                Operation.builder().operationType(OperationType.DEPOSIT).date(TODAY).amount(aAmountOf100).balance(Balance.of(200)).build(),
+                Operation.builder().operationType(OperationType.WITHDRAW).date(TODAY).amount(aAmountOf100).balance(Balance.of(100)).build(),
+                Operation.builder().operationType(OperationType.DEPOSIT).date(TODAY).amount(aAmountOf100).balance(Balance.of(200)).build()));
 
-        bankAccount.withdraw(aAmountOf100);
+        bankAccount.deposit(aAmountOf100);
         bankAccount.deposit(aAmountOf100);
         bankAccount.withdraw(aAmountOf100);
         bankAccount.deposit(aAmountOf100);
