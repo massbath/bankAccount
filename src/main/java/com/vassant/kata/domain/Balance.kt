15d63b22ff99af850a3acd410116427548d6d084
massbath <clement.vassant@gmail.com>
2020-10-07 21:17:56 +0200

    Convert to kotlin Balance

--- src/main/java/com/vassant/kata/domain/Balance.kt
+++ src/main/java/com/vassant/kata/domain/Balance.kt
@@ -1,45 +1,16 @@
-package com.vassant.kata.domain;
+package com.vassant.kata.domain
 
-public final class Balance {
+data class Balance(private val value: Int) {
+    fun deposit(amount: Amount): Balance = Balance(value + amount.value)
 
-    private int value;
+    fun withdraw(amount: Amount): Balance = Balance(value - amount.value)
 
-    private Balance(int value) {
-        this.value = value;
-    }
+    fun hasEnoughSavings(amount: Amount): Boolean = value >= amount.value
 
-    static Balance of(int value) {
-        return new Balance(value);
-    }
-
-    Balance deposit(Amount amount) {
-        return Balance.of(value + amount.getValue());
-    }
-
-    Balance withdraw(Amount amount) {
-        return Balance.of(value - amount.getValue());
-    }
-
-    boolean hasEnoughSavings(Amount amount) {
-        return value >= amount.getValue();
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Balance)) return false;
-        final Balance other = (Balance) o;
-        if (this.value != other.value) return false;
-        return true;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        result = result * PRIME + this.value;
-        return result;
-    }
-
-    public String toString() {
-        return "Balance(value=" + this.value + ")";
+    companion object {
+        @JvmStatic
+        fun of(value: Int): Balance {
+            return Balance(value)
+        }
     }
 }
